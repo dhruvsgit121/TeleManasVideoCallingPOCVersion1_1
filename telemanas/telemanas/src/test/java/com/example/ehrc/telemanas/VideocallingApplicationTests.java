c861f5dfa9f29c4214d123ab45aab4fa225f2a60
dhruviiitb <dhruv.gupta@iiitb.ac.in>
2024-08-05 13:30:44 +0530

    Initial Commit.

---
+++ telemanas/telemanas/src/test/java/com/example/ehrc/telemanas/VideocallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehrc.telemanas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideocallingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
