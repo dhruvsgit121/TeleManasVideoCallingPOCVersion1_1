e23d7c4fd543dbf2237d50e3f601ebdd6c8b2f25
dhruvsgit121 <dhruv.sgit@gmail.com>
2024-08-23 13:23:48 +0530

    Adding the check for blank data in DAO.

--- telemanas/telemanas/src/main/java/com/example/ehrc/telemanas/DTO/AuthenticateUserDTO.java
+++ telemanas/telemanas/src/main/java/com/example/ehrc/telemanas/DTO/AuthenticateUserDTO.java
@@ -1,6 +1,7 @@
 package com.example.ehrc.telemanas.DTO;
 
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,12 +21,15 @@ public class AuthenticateUserDTO {
     }
 
     @NotNull(message = "Telemanas ID cannot be null. Please enter Telemanas ID to proceed.")
+    @NotBlank(message = "Telemanas ID cannot be blank. Please enter Telemanas ID to proceed.")
     private String telemanasId;
 
     @NotNull(message = "UUID cannot be null. Please enter UUID to proceed.")
+    @NotBlank(message = "UUID cannot be blank. Please enter UUID to proceed.")
     private String userUuid;
 
     @NotNull(message = "MHP Username cannot be null. Please enter MHP Username to proceed.")
+    @NotBlank(message = "MHP Username cannot be blank. Please enter MHP Username to proceed.")
     private String mhpUserName;
 
     private String bearerToken;
