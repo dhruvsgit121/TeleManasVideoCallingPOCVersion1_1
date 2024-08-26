727de5dd9be2b64736369c8b152b850c6f11daa7
dhruvsgit121 <dhruv.sgit@gmail.com>
2024-08-26 16:57:43 +0530

    Move EY API URL's to properties file.

--- telemanas/telemanas/src/main/java/com/example/ehrc/telemanas/AuthenticateService/AuthenticateUser.java
+++ telemanas/telemanas/src/main/java/com/example/ehrc/telemanas/AuthenticateService/AuthenticateUser.java
@@ -2,9 +2,12 @@ package com.example.ehrc.telemanas.AuthenticateService;
 
 import com.example.ehrc.telemanas.DTO.AuthenticateUserDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
+
+@Service
 public interface AuthenticateUser {
 
     public ResponseEntity<Map<String, Object>> authenticateUser(AuthenticateUserDTO userData);
--- telemanas/telemanas/src/main/java/com/example/ehrc/telemanas/Utilities/VideoCallingAPIConstants.java
+++ telemanas/telemanas/src/main/java/com/example/ehrc/telemanas/Utilities/VideoCallingAPIConstants.java
@@ -1,15 +1,23 @@
 package com.example.ehrc.telemanas.Utilities;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class VideoCallingAPIConstants {
 
     public static String isErrorFlagValue= "isErrorPresent";
     public static String errorMessageValue= "errorMessage";
 
-    public static String authenticatePatientURL = "patients/get-patient-details";
-    public static String getUserDetailsURL = "user/get-userDetails-by-userName";
+    public static String getUserDetailsURL;
+    public static String decryptPatientMobileNumberURL;
+    public static String authenticatePatientURL;
 
-    public static String decryptPatientMobileNumberURL = "commonservice/common/decrypt-phone-for-iiitb-to-original";
-
-//    /commonservice/common/decrypt-phone-for-iiitb-to-original
-//    decrypt-phone-for-ameyo-to-original
+    public VideoCallingAPIConstants(@Value("${videocalling.api.autheticate.patient}") String authenticatePatientURL,
+                               @Value("${videocalling.api.getuserdetails.username}") String getUserDetailsURL,
+                               @Value("${videocalling.api.decrypt.mobilenumber}") String decryptPatientMobileNumberURL) {
+        this.authenticatePatientURL = authenticatePatientURL;
+        this.getUserDetailsURL = getUserDetailsURL;
+        this.decryptPatientMobileNumberURL = decryptPatientMobileNumberURL;
+    }
 }
